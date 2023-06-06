e10509aea6ca1e186e344ce041439f89b9c8e01c
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-06-06 22:22:10 +0200

    Add Tamagui

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
+// just to ensure TAMAGUI_TARGET is set
+process.env.TAMAGUI_TARGET = 'native';
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ["nativewind/babel"],
+  plugins: [
+    'nativewind/babel',
+    ['transform-inline-environment-variables', {include: ['TAMAGUI_TARGET']}],
+  ],
 };
--- src/bardistry/App.js
+++ src/bardistry/App.js
@@ -1,7 +1,14 @@
-import * as React from 'react';
+import {Suspense} from 'react';
 import {View, Text, useColorScheme} from 'react-native';
-import {NavigationContainer, DefaultTheme, DarkTheme} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {TamaguiProvider} from 'tamagui';
+import tamaguiConfig from '../../tamagui.config';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,19 +16,24 @@ function App(props) {
   const color = useColorScheme();
 
   return (
-    <NavigationContainer theme={color === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name={props.screens[0].name}
-          component={props.screens[0].component}
-        />
-        <Stack.Screen
-          name={props.screens[1].name}
-          component={props.screens[1].component}
-          options={({ route }) => ({ title: route.params.title })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <TamaguiProvider config={tamaguiConfig}>
+      <Suspense>
+        <NavigationContainer
+          theme={color === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack.Navigator>
+            <Stack.Screen
+              name={props.screens[0].name}
+              component={props.screens[0].component}
+            />
+            <Stack.Screen
+              name={props.screens[1].name}
+              component={props.screens[1].component}
+              options={({route}) => ({title: route.params.title})}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Suspense>
+    </TamaguiProvider>
   );
 }
 
