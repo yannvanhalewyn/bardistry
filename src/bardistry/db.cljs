d927e0d66f8222a4ee87567b8efafd4210e16e61
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-06-06 21:54:28 +0200

    Cleanup http call

--- src/bardistry/db.cljs
+++ src/bardistry/db.cljs
@@ -6,14 +6,14 @@
 
 (defonce db (r/atom {}))
 
+(def HOST "192.168.5.180")
+(def PORT 8080)
+
+(defn- api-url [endpoint]
+  (str "http://" HOST ":" PORT "/" endpoint))
+
 (defn load-songs! []
-  (.log js/console "LOADING songs.json")
-  (try
-    (p/let [res (.catch (js/fetch "http://localhost:8080/songs.json")
-                        #(.error js/console ".catch" %))
-            body (.catch (.text res) #(.error js/console "error" %))]
-      (.log js/console "Songs loaded")
-      (swap! db assoc :songs (transit/read body)))
-    (catch js/Error e
-      (.error js/console "CATCH" e)
-      )))
+  (-> (p/let [res (js/fetch (api-url "songs"))
+              body (.text res)]
+        (swap! db assoc :songs (transit/read body)))
+      (p/catch js/console.error)))
