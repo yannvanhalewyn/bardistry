75ce6702e56f128c3cf1c5eec85744a34614f4be
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-06-08 10:46:53 +0200

    Fix unknown song when refreshing

--- src/bardistry/songs.clj
+++ src/bardistry/songs.clj
@@ -3,8 +3,16 @@
    [clojure.java.io :as io]
    [clojure.string :as str]))
 
+(defonce song-ids (atom {}))
+
+(defn- get-id [title artist]
+  (or (get @song-ids [title artist])
+      (let [id (random-uuid)]
+        (swap! song-ids assoc [title artist] id)
+        id)))
+
 (defn- process-song [[title artist & contents]]
-  {:song/id (random-uuid)
+  {:song/id (get-id title artist)
    :song/title title
    :song/artist artist
    :song/sort-artist (str/replace artist (re-pattern "(?i)^\\s*the\\s*") "")
