b6f8f024148a85c9add29093015948b122638eca
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-06-08 10:46:43 +0200

    Persist navigation state

--- src/bardistry/App.js
+++ src/bardistry/App.js
@@ -1,5 +1,6 @@
-import {Suspense} from 'react';
-import {View, Text} from 'react-native';
+import {Suspense, useState, useEffect} from 'react';
+import {Linking, View, Text} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useColorScheme} from 'nativewind';
 import colors from 'tailwindcss/colors';
 
@@ -14,10 +15,45 @@ const Stack = createNativeStackNavigator();
 
 function App(props) {
   const {colorScheme} = useColorScheme();
+  const [isReady, setIsReady] = useState(false);
+  const [initialState, setInitialState] = useState();
+
+  useEffect(() => {
+
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (Platform.OS !== 'web' && initialUrl == null) {
+          const savedStateString = await AsyncStorage.getItem(
+            'bardistry.navigation-state',
+          );
+          if (savedStateString) {
+            setInitialState(JSON.parse(savedStateString));
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    restoreState();
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
 
   return (
     <Suspense>
       <NavigationContainer
+        initialState={initialState}
+        onStateChange={state => {
+          AsyncStorage.setItem(
+            'bardistry.navigation-state',
+            JSON.stringify(state),
+          );
+        }}
         theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack.Navigator>
           <Stack.Screen
@@ -28,8 +64,10 @@ function App(props) {
           <Stack.Screen
             name={props.screens[1].name}
             component={props.screens[1].component}
-            options={({route}) => ({title: route.params.title,
-                                    headerTintColor: colors.orange['500']})}
+            options={({route}) => ({
+              title: route.params.title,
+              headerTintColor: colors.orange['500'],
+            })}
           />
         </Stack.Navigator>
       </NavigationContainer>
