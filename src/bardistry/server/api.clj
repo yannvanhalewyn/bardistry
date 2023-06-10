a27feb8e62ef7c323f680b5dd502af35f6826142
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-06-11 00:14:11 +0200

    Remove unused API routes

--- src/bardistry/server/api.clj
+++ src/bardistry/server/api.clj
@@ -2,21 +2,6 @@
   (:require
    [com.biffweb :as biff]))
 
-(defn- all-songs [{:keys [biff/db]}]
-  {:status 200
-   :body (biff/q db '{:find (pull song [:song/id :song/title :song/artist])
-                      :where [[song :song/id]]})})
-
-(defn- lyrics [{:keys [biff/db path-params] :as req}]
-  (if-let [song (first
-                 (biff/q db '{:find [(pull song [:song/id :song/contents])]
-                              :in [id]
-                              :where [[song :song/id id]]}
-                         (parse-uuid (:id path-params))))]
-    {:status 200
-     :body song}
-    {:status 404}))
-
 (defn- query [{:keys [biff/db params]}]
   {:status 200
    :body (apply biff/q db (:query params) (:params params))})
@@ -44,7 +29,5 @@
 
 (def plugin
   {:schema schema
-   :api-routes [["/api/songs" {:get all-songs}]
-                ["/api/songs/:id/lyrics" {:get lyrics}]
-                ["/api/q" {:post query}]
+   :api-routes [["/api/q" {:post query}]
                 ["/api/mutate" {:post mutate}]]})
