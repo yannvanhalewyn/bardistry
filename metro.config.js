40b2254b531ce1e5fc5eacbd040fbcf235e98413
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-06-15 06:36:25 +0200

    Remove unused metro-symlinked-deps

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,4 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const {
-  applyConfigForLinkedDependencies,
-} = require('@carimus/metro-symlinked-deps');
 
 /**
  * Metro configuration
@@ -11,6 +8,4 @@ const {
  */
 const config = {};
 
-module.exports = applyConfigForLinkedDependencies(
-  mergeConfig(getDefaultConfig(__dirname), config),
-);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
